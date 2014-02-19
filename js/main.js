a8a3e8d6a521647e4b738de34ef078840655d4ad
billey <guillaume.billey@inist.fr>
2014-02-19 16:18:00 +0100

    Récupération et affichage du résultat

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var url = "http://api.istex.fr/?";
 
+require(["js/conf","js/vendor/mustache"], function(conf, mustache) {
+
+    function displayResults(data) {
+        var tableLine = "{{#hits}}<tr><td>{{title}}<td><td></td><td></td></tr>{{/hits}}"
+        $("#tableResult").html(mustache.render(tableLine, data));
+    }
+
+    $("#searchform").submit(function (event) {
+        event.preventDefault();
+
+        var keywords = $("#searchfield").val();
+        var query = "?q=" + keywords;
+        var request = {
+            url: conf.apiUrl + query,
+            jsonp: true,
+            crossDomain: true,
+            success: displayResults
+        };
+
+        $.ajax(request);
+    });
+});
