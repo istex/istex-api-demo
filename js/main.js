9284971cd7d4b1f60109e3b4f7a91e6cd0205209
billey <guillaume.billey@inist.fr>
2014-02-20 16:24:27 +0100

    Ajout du loading + retouches CSS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ require(["js/conf", "js/vendor/mustache"], function (conf, mustache) {
     "use strict";
 
     var resultsPerPage = 10;
-    var currentPage = 1;
+    var currentPage = 0;
     var keywords = "";
     var title = "";
     var numberOfPages = 0;
@@ -15,12 +15,14 @@ require(["js/conf", "js/vendor/mustache"], function (conf, mustache) {
 
     $("#searchform").submit(function (event) {
         event.preventDefault();
+        currentPage = 1;
         keywords = $("#searchfield").val();
         search();
     });
 
     $("#advancedSearchform").submit(function (event) {
         event.preventDefault();
+        currentPage = 1;
         title = $("#titleField").val();
         advancedSearch();
     });
@@ -63,10 +65,10 @@ require(["js/conf", "js/vendor/mustache"], function (conf, mustache) {
 
     function displayResults(data) {
         numberOfResults = data.total;
-        numberOfPages = numberOfResults === 0 ? "*" : Math.ceil(numberOfResults / resultsPerPage);
-        currentPage = numberOfResults === 0 ? "*" : 1;
-        $("#currentPage").text(currentPage);
-        $("#totalPages").text(numberOfPages);
+        numberOfPages = numberOfResults === 0 ? 0 : Math.ceil(numberOfResults / resultsPerPage);
+        currentPage = numberOfResults === 0 ? 0 : currentPage;
+        $("#currentPage").text(currentPage === 0 ? "*" : currentPage);
+        $("#totalPages").text(numberOfPages === 0 ? "*" : numberOfPages);
 
         var linksTemplates = {
             fulltext: "<a href=\"" + conf.apiUrl + "/{{id}}/fulltext/original\" target=\"_blank\"><span class=\"glyphicon glyphicon-file\"></span></a>",
@@ -75,26 +77,46 @@ require(["js/conf", "js/vendor/mustache"], function (conf, mustache) {
         var tableLine = "{{#hits}}<tr><td>{{title}}</td><td style='text-align: center;'>{{> fulltext}}</td><td style='text-align: center;'>{{> metadata}}</td></tr>{{/hits}}";
 
         $("#tableResult").html(mustache.to_html(tableLine, data, linksTemplates));
-        $("#searchButton").button('reset');
+        $("button").button('reset');
+        $(".loading").hide();
+    }
+
+    function manageError(err){
+        $("button").button('reset');
+        $(".loading").hide();
+        alert("Houston ... Problem!");
     }
 
     function search() {
         var query = "?q=" + keywords;
         query += "&size=" + resultsPerPage;
-        query += "&from=" + resultsPerPage * (currentPage - 1);
+        query += "&from=" + resultsPerPage * (currentPage === 0 ? 1 : currentPage - 1);
         $("#searchButton").button('loading');
+        $(".loading").show();
         var request = {
             url: conf.apiUrl + query,
             jsonp: true,
             crossDomain: true,
-            success: displayResults
+            success: displayResults,
+            error : manageError
         };
 
         $.ajax(request);
     }
 
     function advancedSearch() {
+        var query = "?q=title:" + title;
+        $("#advancedSearchButton").button('loading');
+        $(".loading").show();
+        var request = {
+            url: conf.apiUrl + query,
+            jsonp: true,
+            crossDomain: true,
+            success: displayResults,
+            error : manageError
+        };
 
+        $.ajax(request);
 
     }
 
