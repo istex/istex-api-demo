3bb1f7d3a5d8786aee39137efa1786b0a9631ed3
billey <guillaume.billey@inist.fr>
2014-02-20 14:27:27 +0100

    Ajustement du style + début de la recherche avancée

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-require(["js/conf","js/vendor/mustache"], function(conf, mustache) {
+require(["js/conf", "js/vendor/mustache"], function (conf, mustache) {
     "use strict";
 
     var resultsPerPage = 10;
     var currentPage = 1;
     var keywords = "";
-    var numberOfPages=0;
-    var numberOfResults=0;
-
+    var title = "";
+    var numberOfPages = 0;
+    var numberOfResults = 0;
 
 
     /******************************************
@@ -15,37 +15,42 @@ require(["js/conf","js/vendor/mustache"], function(conf, mustache) {
 
     $("#searchform").submit(function (event) {
         event.preventDefault();
-        currentPage = 1;
         keywords = $("#searchfield").val();
         search();
     });
 
-    $("#prev").click(function(){
-        if (currentPage <= 1){
-           return;
+    $("#advancedSearchform").submit(function (event) {
+        event.preventDefault();
+        title = $("#titleField").val();
+        advancedSearch();
+    });
+
+    $("#prev").click(function () {
+        if (currentPage <= 1) {
+            return;
         }
         currentPage--;
         search();
     });
 
-    $("#first").click(function(){
-        if (currentPage <= 1){
+    $("#first").click(function () {
+        if (currentPage <= 1) {
             return;
         }
         currentPage = 1;
         search();
     });
 
-    $("#next").click(function(){
-        if (currentPage >= numberOfPages){
+    $("#next").click(function () {
+        if (currentPage >= numberOfPages) {
             return;
         }
         currentPage++;
         search();
     });
 
-    $("#last").click(function(){
-        if (currentPage-1 >= numberOfPages){
+    $("#last").click(function () {
+        if (currentPage - 1 >= numberOfPages) {
             return;
         }
         currentPage = numberOfPages;
@@ -58,20 +63,26 @@ require(["js/conf","js/vendor/mustache"], function(conf, mustache) {
 
     function displayResults(data) {
         numberOfResults = data.total;
-        numberOfPages = Math.ceil(numberOfResults/resultsPerPage);
+        numberOfPages = numberOfResults === 0 ? "*" : Math.ceil(numberOfResults / resultsPerPage);
+        currentPage = numberOfResults === 0 ? "*" : 1;
+        $("#currentPage").text(currentPage);
+        $("#totalPages").text(numberOfPages);
 
         var linksTemplates = {
-            fulltext : "<a href=\""+conf.apiUrl+"/{{id}}/fulltext/original\" target=\"_blank\"><span class=\"glyphicon glyphicon-file\"></span></a>",
-            metadata : "<a href=\""+conf.apiUrl+"/{{id}}/metadata/original\" target=\"_blank\"><span class=\"glyphicon glyphicon-align-center\"></span></a>"};
+            fulltext: "<a href=\"" + conf.apiUrl + "/{{id}}/fulltext/original\" target=\"_blank\"><span class=\"glyphicon glyphicon-file\"></span></a>",
+            metadata: "<a href=\"" + conf.apiUrl + "/{{id}}/metadata/original\" target=\"_blank\"><span class=\"glyphicon glyphicon-align-center\"></span></a>"};
 
-        var tableLine = "{{#hits}}<tr><td>{{title}}</td><td>{{> fulltext}}</td><td>{{> metadata}}</td></tr>{{/hits}}";
+        var tableLine = "{{#hits}}<tr><td>{{title}}</td><td style='text-align: center;'>{{> fulltext}}</td><td style='text-align: center;'>{{> metadata}}</td></tr>{{/hits}}";
+
         $("#tableResult").html(mustache.to_html(tableLine, data, linksTemplates));
+        $("#searchButton").button('reset');
     }
 
     function search() {
         var query = "?q=" + keywords;
         query += "&size=" + resultsPerPage;
         query += "&from=" + resultsPerPage * (currentPage - 1);
+        $("#searchButton").button('loading');
         var request = {
             url: conf.apiUrl + query,
             jsonp: true,
@@ -81,4 +92,10 @@ require(["js/conf","js/vendor/mustache"], function(conf, mustache) {
 
         $.ajax(request);
     }
+
+    function advancedSearch() {
+
+
+    }
+
 });
