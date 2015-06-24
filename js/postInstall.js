c96028ccd1421ac5dd703bbd67de7bef267ac919
parentin <jean-joffrey.parentin@inist.fr>
2015-06-24 17:37:02 +0200

    Modif post install

--- js/postInstall.js
+++ js/postInstall.js
@@ -1,30 +1,57 @@
+/* global process */
+
+"use strict";
+
 var fs = require("fs"),
-  _ = require("lodash")
+  _ = require("lodash"),
+  finalParameters = {},
+  envParameters,
+  parameters,
+  distParameters,
+  generatedScript,
+  data
   ;
 
-fs.readFile("js/parameters.json", function (err, data) {
-//  if (err) {
-//    return console.log(err);
-//  }
+// parametres d'environnement
+if (process.env.ISTEX_DEMO_PARAMETERS_FILE && typeof process.env.ISTEX_DEMO_PARAMETERS_FILE === 'string') {
+  data = fs.readFileSync(process.env.ISTEX_DEMO_PARAMETERS_FILE);
+} else {
+  console.log("Pas de paramètres d'environnement");
+}
 
-  var parameters = JSON.parse(data);
+envParameters = data && JSON.parse(data) || {};
 
-  fs.readFile("js/parameters.json.dist", function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
 
-    var distParameters = JSON.parse(data),
-      parametersFile
-      ;
+// parametres locaux
+try {
+  data = fs.readFileSync("js/parameters.json");
+} catch(e) {
+  if (e.code === 'ENOENT') {
+    console.info("Pas de paramètres locaux");
+  } else {
+    console.error("Erreur lors de la lecture du fichier parameters.json: ", e);
+    throw e;
+  }
+}
+parameters = data && JSON.parse(data) || {};
 
-    _.defaults(parameters, distParameters);
-    parametersFile = "define(" + JSON.stringify(parameters) + ");";
 
-    fs.writeFile("js/parameters.js", parametersFile, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-    });
-  });
+// parametres distants
+data = fs.readFileSync("js/parameters.json.dist");
+distParameters = data && JSON.parse(data);
+
+// On cascade les parametres
+_.defaults(finalParameters, envParameters);
+_.defaults(finalParameters, parameters);
+_.defaults(finalParameters, distParameters);
+
+generatedScript =
+  "/* Ce fichier est généré par npm, merci de ne pas le modifier */ \n "
+  + "define(" + JSON.stringify(finalParameters) + ");";
+
+fs.writeFile("js/parameters.js", generatedScript, function (err) {
+  if (err) {
+    throw err;
+  }
 });
+
