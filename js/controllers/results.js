b820f94b83897cfc6fc378c2dbedcb5d21a5a7b8
istexdepl <istex@inist.fr>
2016-02-08 15:19:22 +0100

    Refactoring sur results.js

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -68,97 +68,10 @@ define(["config", "vendor/mustache", "text!views/resultRow.html"], function(conf
         $("#totalResults").val(data.total);
         $("#totalms").val(data.stats.elasticsearch.took + data.stats['istex-api'].took);
 
-        // On wrap l'objet data avant de lui donner de nouvelles méthodes.
-        data = Object.create(data);
-        data.abstr = function() {
-          return function(text, render) {
-            if (render(text) === "") {
-              return "Pas de résumé pour ce résultat.";
-            }
-            return render(text);
-          };
-        };
+        // Ajoute les fonctions nécessaires à data
+        data = generateDataFunctions(data, config);
 
-        data.mimetypeIconName = (function(config) {
-          return function() {
-            return config.mimetypeIconNames[this.mimetype] || config.mimetypeIconNames["unknown"];
-          };
-        }(config));
-
-        data.spaceless = function() {
-          return function(text, render) {
-            return render(text).replace(/(?:>)\s*(?=<)/g, ">");
-          };
-        };
-
-        data.hasFulltext = function() {
-          return this.fulltext && this.fulltext.length;
-        };
-
-        data.hasMetadata = function() {
-          return this.metadata && this.metadata.length;
-        };
-
-        data.hasCovers = function() {
-          return this.covers && this.covers.length;
-        };
-
-        data.hasAnnexes = function() {
-          return this.annexes && this.annexes.length;
-        };
-
-        data.hasEnrichments = function() {
-          return this.enrichments && this.enrichments.length;
-        };
-
-        data.consolidateEnrichmentsUri = function() {
-          if (!this.enrichments) {
-            return;
-          }
-
-          var path = [];
-          this.enrichments.forEach(function(enrichment) {
-            path.push(enrichment.type);
-          });
-          return 'https://api.istex.fr/document/' + this.id + '/enrichments/' + path.join(',') + '?consolidate';
-        };
-
-        data.titleClic = function() {
-          return function(text, render) {
-            var res = render(text),
-              infos = res.split(" "),
-              index = infos.indexOf("application/pdf"),
-              title = res.slice(res.indexOf("\"") + 1, res.length - 1);
-
-            if (index !== -1) {
-              return "<a href=\"" + infos[index + 1] + "\" target=\"_blank\">" + title + "</a>";
-            }
-
-            return title;
-          };
-        };
-
-        data.quality = function() {
-          return function(text, render) {
-            if (render(text).split(':')[1] === " ") {
-              return "";
-            }
-            return "<div class='text-right'><b class='label label-info'>" + render(text) + "</b>";
-          };
-        };
-
-        data.presence = function() {
-          return function(text, render) {
-            var res = render(text);
-            if (res === 'T') {
-              return "Présente(s)";
-            } else {
-              return "Absente(s)";
-            };
-          };
-        };
-
-        var template, lang, wos, obj;
+        var lang, wos, obj;
         var languageList = [];
         var wosList = [];
 
@@ -175,47 +88,11 @@ define(["config", "vendor/mustache", "text!views/resultRow.html"], function(conf
         $('#nbWOSResults').text('');
         $('#sortMenu:first-child').html('Tri par : Qualité <span class="caret"></span>');
 
-        // CorpusFacet
-        template = "{{#aggregations.corpusName.buckets}}<div class='col-xs-offset-1'>" +
-          "<div class='checkbox'><label><input value={{key}} type='checkbox'>{{key}}</label>" +
-          "<span class='badge pull-right'>{{docCount}}</span></div></div>{{/aggregations.corpusName.buckets}}";
-        $('#nbCorpusFacet').text(data.aggregations.corpusName.buckets.length);
-        $('#facetCorpus').append(mustache.render(template, data));
-        if (data.aggregations.corpusName.buckets.length === 1) {
-          $('#facetCorpus').get(0).getElementsByTagName('input').item(0).checked = true;
-          $('#facetCorpus').get(0).getElementsByTagName('input').item(0).disabled = true;
-        }
-
-        // ArticleTypeFacet
-        template = "{{#aggregations.genre.buckets}}<div class='col-xs-offset-1'>" +
-          "<div class='checkbox'><label><input value={{key}} type='checkbox'>{{key}}</label>" +
-          "<span class='badge pull-right'>{{docCount}}</span></div></div>{{/aggregations.genre.buckets}}";
-        $('#nbArticleTypeFacet').text(data.aggregations.genre.buckets.length);
-        $('#facetArticleType').append(mustache.render(template, data));
-        if (data.aggregations.genre.buckets.length === 1) {
-          $('#facetArticleType').get(0).getElementsByTagName('input').item(0).checked = true;
-          $('#facetArticleType').get(0).getElementsByTagName('input').item(0).disabled = true;
-        }
-
-        // PDFVersionFacet
-        template = "{{#aggregations.pdfVersion.buckets}}<div class='col-xs-offset-1 col-xs-10'>" +
-          "<div class='checkbox'><label><input value={{key}} type='checkbox'>{{key}}</label>" +
-          "<span class='badge pull-right'>{{docCount}}</span></div></div>{{/aggregations.pdfVersion.buckets}}";
-        $('#facetPDFVersion').append(mustache.render(template, data));
-        if (data.aggregations.pdfVersion.buckets.length === 1) {
-          $('#facetPDFVersion').get(0).getElementsByTagName('input').item(0).checked = true;
-          $('#facetPDFVersion').get(0).getElementsByTagName('input').item(0).disabled = true;
-        }
-
-        // RefBibsNativeFacet
-        template = "{{#aggregations.refBibsNative.buckets}}<div class='col-xs-offset-1'>" +
-          "<div class='checkbox'><label><input value={{key}} type='checkbox'>{{#presence}}{{key}}{{/presence}}</label>" +
-          "<span class='badge pull-right'>{{docCount}}</span></div></div>{{/aggregations.refBibsNative.buckets}}";
-        $('#facetRefBibsNative').append(mustache.render(template, data));
-        if (data.aggregations.refBibsNative.buckets.length === 1) {
-          $('#facetRefBibsNative').get(0).getElementsByTagName('input').item(0).checked = true;
-          $('#facetRefBibsNative').get(0).getElementsByTagName('input').item(0).disabled = true;
-        }
+        // Génération des facettes de type "terms"
+        generateTermsFacet('corpusName', '{{key}}', $('#facetCorpus'), $('#nbCorpusFacet'), data, mustache);
+        generateTermsFacet('genre', '{{key}}', $('#facetArticleType'), $('#nbArticleTypeFacet'), data, mustache);
+        generateTermsFacet('pdfVersion', '{{key}}', $('#facetPDFVersion'), null, data, mustache);
+        generateTermsFacet('refBibsNative', '{{#presence}}{{key}}{{/presence}}', $('#facetRefBibsNative'), null, data, mustache);
 
         // LanguageFacet
         for (lang of data.aggregations.language.buckets) {
@@ -226,14 +103,7 @@ define(["config", "vendor/mustache", "text!views/resultRow.html"], function(conf
           if (obj.label === undefined) obj.label = obj.value;
           languageList.push(obj);
         }
-
-        $("#languages").autocomplete("option", "source", languageList)
-          .autocomplete('instance')._renderItem = function(ul, item) {
-            return $('<li>')
-              .append('<a>' + item.label + "<br><span style=\"font-size:10px;\">" + item.desc + '</span></a>')
-              .appendTo(ul);
-          };
-        $('#nbLangFacet').text(data.aggregations.language.buckets.length);
+        generateAutocompleteFacet($("#languages"), languageList, $('#nbLangFacet'), 'language', data);
 
         // WosFacet
         for (wos of data.aggregations.wos.buckets) {
@@ -243,24 +113,13 @@ define(["config", "vendor/mustache", "text!views/resultRow.html"], function(conf
           obj.label = wos.key;
           wosList.push(obj);
         }
+        generateAutocompleteFacet($("#wosCategories"), wosList, $('#nbWOSFacet'), 'wos', data);
 
-        $("#wosCategories").autocomplete("option", "source", wosList)
-          .autocomplete("instance")._renderItem = function(ul, item) {
-            return $("<li>")
-              .append("<a>" + item.label + "<br><span style=\"font-size:10px;\">" + item.desc + "</span></a>")
-              .appendTo(ul);
-          };
-        $('#nbWOSFacet').text(data.aggregations.wos.buckets.length);
-
-        // ScoreFacet
+        // Appel des displayRanges
         this.displayRanges(data, "score", "#slider-range-score", "#amountScore", '', 'float');
-        // CopyrightDateFacet
         this.displayRanges(data, "copyrightDate", "#slider-range-copyright", "#amountCopyrightDate", '#nbCopyrightFacet', 'date');
-        // PubDateFacet
         this.displayRanges(data, "publicationDate", "#slider-range-pubdate", "#amountPubDate", '#nbPublicationFacet', 'date');
-        // PdfWordCountFacet
         this.displayRanges(data, "pdfWordCount", "#slider-range-PDFWordCount", "#amountPDFWordCount", '', 'integer');
-        // PdfCharCountFacet
         this.displayRanges(data, "pdfCharCount", "#slider-range-PDFCharCount", "#amountPDFCharCount", '', 'integer');
 
       } else {
@@ -272,9 +131,7 @@ define(["config", "vendor/mustache", "text!views/resultRow.html"], function(conf
         $("#currentPage").text("*");
         $("#totalPages").text("*");
 
-        if (!searchPage.reaffine) {
-          $('#accordeon').hide();
-        }
+        if (!searchPage.reaffine) $('#accordeon').hide();
       }
 
       $("button").button('reset');
@@ -282,3 +139,121 @@ define(["config", "vendor/mustache", "text!views/resultRow.html"], function(conf
     }
   };
 });
+
+function generateDataFunctions(data, config) {
+  // On wrap l'objet data avant de lui donner de nouvelles méthodes.
+  data = Object.create(data);
+
+  data.abstr = function() {
+    return function(text, render) {
+      if (render(text) === "") {
+        return "Pas de résumé pour ce résultat.";
+      }
+      return render(text);
+    };
+  };
+
+  data.mimetypeIconName = (function(config) {
+    return function() {
+      return config.mimetypeIconNames[this.mimetype] || config.mimetypeIconNames["unknown"];
+    };
+  }(config));
+
+  data.spaceless = function() {
+    return function(text, render) {
+      return render(text).replace(/(?:>)\s*(?=<)/g, ">");
+    };
+  };
+
+  data.hasFulltext = function() {
+    return this.fulltext && this.fulltext.length;
+  };
+
+  data.hasMetadata = function() {
+    return this.metadata && this.metadata.length;
+  };
+
+  data.hasCovers = function() {
+    return this.covers && this.covers.length;
+  };
+
+  data.hasAnnexes = function() {
+    return this.annexes && this.annexes.length;
+  };
+
+  data.hasEnrichments = function() {
+    return this.enrichments && this.enrichments.length;
+  };
+
+  data.consolidateEnrichmentsUri = function() {
+    if (!this.enrichments) {
+      return;
+    }
+
+    var path = [];
+    this.enrichments.forEach(function(enrichment) {
+      path.push(enrichment.type);
+    });
+    return 'https://api.istex.fr/document/' + this.id + '/enrichments/' + path.join(',') + '?consolidate';
+  };
+
+  data.titleClic = function() {
+    return function(text, render) {
+      var res = render(text),
+        infos = res.split(" "),
+        index = infos.indexOf("application/pdf"),
+        title = res.slice(res.indexOf("\"") + 1, res.length - 1);
+
+      if (index !== -1) {
+        return "<a href=\"" + infos[index + 1] + "\" target=\"_blank\">" + title + "</a>";
+      }
+
+      return title;
+    };
+  };
+
+  data.quality = function() {
+    return function(text, render) {
+      if (render(text).split(':')[1] === " ") {
+        return "";
+      }
+      return "<div class='text-right'><b class='label label-info'>" + render(text) + "</b>";
+    };
+  };
+
+  data.presence = function() {
+    return function(text, render) {
+      var res = render(text);
+      if (res === 'T') {
+        return "Présente(s)";
+      } else {
+        return "Absente(s)";
+      };
+    };
+  };
+
+  return data;
+}
+
+function generateTermsFacet(facetName, keys, tag, nbTag, data, mustache) {
+  var template = "{{#aggregations." + facetName + ".buckets}}<div class='col-xs-offset-1'>" +
+    "<div class='checkbox'><label><input value={{key}} type='checkbox'>" + keys + "</label>" +
+    "<span class='badge pull-right'>{{docCount}}</span></div></div>{{/aggregations." + facetName + ".buckets}}";
+
+  if (nbTag) nbTag.text(data.aggregations[facetName].buckets.length);
+  tag.append(mustache.render(template, data));
+  if (data.aggregations[facetName].buckets.length === 1) {
+    tag.get(0).getElementsByTagName('input').item(0).checked = true;
+    tag.get(0).getElementsByTagName('input').item(0).disabled = true;
+  }
+}
+
+function generateAutocompleteFacet(tag, list, nbTag, facetName, data) {
+  tag.autocomplete("option", "source", list)
+    .autocomplete('instance')._renderItem = function(ul, item) {
+      return $('<li>')
+        .append('<a>' + item.label + "<br><span style=\"font-size:10px;\">" + item.desc + '</span></a>')
+        .appendTo(ul);
+    };
+  nbTag.text(data.aggregations[facetName].buckets.length);
+}
