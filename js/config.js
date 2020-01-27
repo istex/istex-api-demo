a857949f9cdbb39595466978673361a945e0b102
niederle <claude.niederlender@inist.fr>
2020-01-27 17:42:38 +0100

    bad version number

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 define(["js/parameters.js"], function(parameters) {
   return {
     apiUrl: parameters[parameters.default_api_url],
-    version: "1.8.1",
+    version: "1.8.2",
     mimetypeIconNames: {
       "application/zip": "zip",
       "application/pdf": "pdf",
